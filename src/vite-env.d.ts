069b986b20d4bd9af7774cb68dbed9de5f6727f1
дмитрий <dmitrijkopejka5@gmail.com>
2025-11-09 17:36:04 +0300

    first commit

---
+++ src/vite-env.d.ts
@@ -0,0 +1,9 @@
+typescript
+/// <reference types="svelte" />
+/// <reference types="vite/client" />
+
+declare module '*.svelte' {
+  import type { ComponentType } from 'svelte'
+  const component: ComponentType
+  export default component
+}
